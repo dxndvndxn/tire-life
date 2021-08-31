89b86b3f9d053b6afdeb904fbb0a50879743dc40
dxndvndxn <danjablooo1@gmail.com>
2021-09-01 01:09:13 +0300

    fist commit

---
+++ components/common/layout/header.vue
@@ -0,0 +1,51 @@
+<template>
+  <nav>
+    <ul>
+      <li v-for="(item, i) in menu" :key="i">
+        {{ item }}
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'header',
+  data: () => ({
+    menu: [
+      {
+        view: 'Шины',
+        link: 'Tires'
+      },
+      {
+        view: 'Диски',
+        link: 'Disks'
+      },
+      {
+        view: 'Шиномонтаж',
+        link: 'TireFitting'
+      },
+      {
+        view: 'Хранение',
+        link: 'Storage'
+      },
+      {
+        view: 'Доставка',
+        link: 'Delivery'
+      },
+      {
+        view: 'Акции',
+        link: 'Sale'
+      },
+      {
+        view: 'Контакты',
+        link: 'Contacts'
+      }
+    ]
+  })
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <HeaderBar />
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+import HeaderBar from '../components/common/layout/header'
+export default {
+  name: 'Default',
+  components: {
+    HeaderBar
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
